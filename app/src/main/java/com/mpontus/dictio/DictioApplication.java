134ae010d9a58e95cbdeb8232793e984e8ddf4fe
Mikhail Pontus <m.pontus@gmail.com>
2018-07-01 11:47:04 +0300

    Install LeakCanary and enable StrictMode

--- app/src/main/java/com/mpontus/dictio/DictioApplication.java
+++ app/src/main/java/com/mpontus/dictio/DictioApplication.java
@@ -1,6 +1,9 @@
 package com.mpontus.dictio;
 
+import android.os.StrictMode;
+
 import com.mpontus.dictio.di.DaggerApplicationComponent;
+import com.squareup.leakcanary.LeakCanary;
 
 import dagger.android.AndroidInjector;
 import dagger.android.support.DaggerApplication;
@@ -13,6 +16,20 @@ public class DictioApplication extends DaggerApplication {
 
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
+
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyDeath()
+                    .build());
+
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyDeath()   // If violations are in Android itself or 3rd-party libs, use penaltyLog.
+                    .build());
+
+            if (!LeakCanary.isInAnalyzerProcess(this)) {
+                LeakCanary.install(this);
+            }
         }
     }
 
