03b7da61bb94437a7deca4565e4d095e6d7e1840
Mikhail Pontus <m.pontus@gmail.com>
2018-07-15 16:07:48 +0300

    Try to implement backend

---
+++ backend/src/main/java/com/mpontus/dictio/backend/Message.java
@@ -0,0 +1,14 @@
+package com.mpontus.dictio.backend;
+
+public class Message {
+
+    private String message;
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
