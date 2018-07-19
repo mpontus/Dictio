03f8210d4ccda3523ad7e5e1c84b0952b5901cb1
Mikhail Pontus <m.pontus@gmail.com>
2018-07-19 17:24:02 +0300

    Add widget on click listener

--- app/src/main/java/com/mpontus/dictio/ui/widget/DictioWidget.java
+++ app/src/main/java/com/mpontus/dictio/ui/widget/DictioWidget.java
@@ -1,8 +1,10 @@
 package com.mpontus.dictio.ui.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.BitmapDrawable;
 import android.widget.RemoteViews;
 
@@ -10,6 +12,7 @@ import com.mpontus.dictio.R;
 import com.mpontus.dictio.data.PromptsRepository;
 import com.mpontus.dictio.data.TranslationManager;
 import com.mpontus.dictio.data.model.Prompt;
+import com.mpontus.dictio.ui.lesson.LessonActivity;
 import com.mpontus.dictio.ui.shared.LangaugeResources;
 
 import javax.inject.Inject;
@@ -38,6 +41,11 @@ public class DictioWidget extends AppWidgetProvider {
         if (prompt != null) {
             // Construct the RemoteViews object
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.dictio_widget);
+
+            Intent intent =
+                    LessonActivity.createIntent(context, prompt.getLanguage(), prompt.getCategory());
+
+            views.setOnClickPendingIntent(R.id.root, PendingIntent.getActivity(context, 0, intent, 0));
 
             views.setTextViewText(R.id.prompt, prompt.getText());
             views.setTextViewText(R.id.translation, translationManager.getTranslation(prompt));
