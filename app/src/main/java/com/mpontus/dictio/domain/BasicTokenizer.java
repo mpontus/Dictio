ce694fa85ac17f854de6ce3803840257d7f204a3
Mikhail Pontus <m.pontus@gmail.com>
2018-08-01 12:35:32 +0300

    Improve BasicTokenizer

--- app/src/main/java/com/mpontus/dictio/domain/BasicTokenizer.java
+++ app/src/main/java/com/mpontus/dictio/domain/BasicTokenizer.java
@@ -7,7 +7,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class BasicTokenizer implements PhraseMatcher.Tokenizer {
-    private static Pattern WORD_PATTERN = Pattern.compile("(?i)([\\u3000-\\u30ff\\uff00-\\uffef\\u4e00-\\u9faf]|[\\w'-]+)");
+    // Japanese:
+    // 3000-303F : punctuation
+    // 3040-309F : hiragana
+    // 30A0-30FF : katakana
+    // FF00-FFEF : Full-width roman + half-width katakana
+    // 4E00-9FAF : Common and uncommon kanji
+    private static Pattern WORD_PATTERN = Pattern.compile("(?i)([\\u3040-\\u30ff\\uff00-\\uffef\\u4e00-\\u9faf]|[\\w'’-]+)");
 
     @Override
     public Collection<PhraseMatcher.Token> tokenize(String phrase) {
