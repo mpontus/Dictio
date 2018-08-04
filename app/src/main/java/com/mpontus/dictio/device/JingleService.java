b23e26d02580f17b57836f1a8980edb3d04c7e5e
Mikhail Pontus <m.pontus@gmail.com>
2018-08-04 20:12:45 +0300

    Introduce JingleService

---
+++ app/src/main/java/com/mpontus/dictio/device/JingleService.java
@@ -0,0 +1,60 @@
+package com.mpontus.dictio.device;
+
+import android.content.res.AssetFileDescriptor;
+import android.content.res.Resources;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+
+import com.mpontus.dictio.R;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+
+import timber.log.Timber;
+
+/**
+ * A service for playing jingles: small sound bits that react to user action
+ */
+public class JingleService {
+    private final MediaPlayer mediaPlayer = new MediaPlayer();
+
+    private final Resources resources;
+
+    @Inject
+    public JingleService(AudioManager audioManager, Resources resources) {
+        this.resources = resources;
+
+        mediaPlayer.setAudioSessionId(audioManager.generateAudioSessionId());
+    }
+
+    public void playSuccess() {
+        play(R.raw.victory, 0.12f);
+    }
+
+    public void playIn() {
+        play(R.raw.in, 0.12f);
+    }
+
+    public void playOut() {
+        play(R.raw.out, 0.04f);
+    }
+
+    public void play(int resId) {
+        play(resId, 0.12f);
+    }
+
+    public void play(int resId, float volume) {
+        mediaPlayer.reset();
+        AssetFileDescriptor fd = resources.openRawResourceFd(resId);
+
+        try {
+            mediaPlayer.setDataSource(fd.getFileDescriptor(), fd.getStartOffset(), fd.getLength());
+            mediaPlayer.setVolume(volume, volume);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            Timber.e(e);
+        }
+    }
+}
